90d6df12581a9e19de87b650e7896ccca90f4989
ColinGreenidge <cgreenidge8896@conestogac.on.ca>
2025-11-20 11:41:07 -0500

    working docker container
    run docker compose up --build, then localhost:3000/api/test to test.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
 import { pool } from "./config/db.js";
-import { execSync } from "child_process";
-import os from "os";
 import bcrypt from "bcrypt";
 
 dotenv.config();
